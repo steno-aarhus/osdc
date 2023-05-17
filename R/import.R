de49ccb3490596e940758b95bf3b41d980fa04f7
Luke W. Johnston <lwjohnst@gmail.com>
2023-05-17 14:24:01 -0400

    add roxygen documentation

--- R/import.R
+++ R/import.R
@@ -1,10 +1,27 @@
-# User specifies the directory of the folder with the raw SAS-files and which file names to be imported. Script then imports all the necessary files:
 
-# Function for importing SAS to project:
+#' A thin wrapper around the [haven::read_sas()] function.
+#'
+#' @param path Path to SAS file location.
+#'
+#' @return A data frame.
+#' @export
+#'
 import_sas <- function(path) {
   haven::read_sas(data_file = path)
 }
 
+#' Save data as an RDS file.
+#'
+#' Compress can be set to TRUE to save space immediately (at the cost of speed
+#' in filtering steps. Using compress steps after running filtering steps is
+#' more optimal.
+#'
+#' @param data The data to save.
+#' @param file The output file name for the RDS file.
+#'
+#' @return Outputs the file path, side effect of saving the data to RDS.
+#' @export
+#'
 save_rds <- function(data, file) {
   saveRDS(data, file, compress = TRUE)
   return(invisible(path))
@@ -18,7 +35,6 @@ output_path <- function(input_path, output_dir) {
   fs::path(output_dir, fs::path_file(input_path))
 }
 
-# Compress can be set to TRUE to save space immediately (at the cost of speed in filter.R. Using compress.R after running filter.R is more optimal.)
 
 # Set in master.R:
 # Regular expression to specify the files to import:
@@ -27,9 +43,18 @@ output_path <- function(input_path, output_dir) {
 
 # Execute the importation:
 
-# To set parallel computing, use future::plan(future::multisession) *outside* of
-# script/function.
-# Import and save all SAS files
+#' Import and save all SAS files into RDS format.
+#'
+#' To speed things up, install the `furrr` and use [future::plan()] with
+#' [future::multisession()] before running the function to set up parallel
+#' processing.
+#'
+#' @param sas_files A vector of SAS file paths.
+#' @param output_dir The folder you want to save the data to.
+#'
+#' @return Save RDS file, invisibly return the data frame.
+#' @export
+#'
 sas_to_rds_files <- function(sas_files, output_dir) {
   save_walk <- purrr::walk2
   import_map <- purrr::map2
@@ -42,7 +67,7 @@ sas_to_rds_files <- function(sas_files, output_dir) {
     sas_files = sas_file_list,
     output_files = sas_file_list |>
       path_set_rds_ext() |>
-      output_path(output_dir = here::here("data-raw"))
+      output_path(output_dir = output_dir)
   ) |>
     import_map(~ list(data = import_sas(path = .x), output_files = .y))
 
