ea8e089ffeab39b7250942367646077caa589894
Signe Kirk Brødbæk <signekb@clin.au.dk>
2023-12-10 13:21:16 +0100

    refactor: move functions to create test lab and hi data into create_test_data.R and delete empty functions.R

--- R/create_test_data.R
+++ R/create_test_data.R
@@ -6,9 +6,6 @@ library(data.table)
 library(tidyverse)
 library(here)
 
-# Load functions
-source(here::here("R/functions.R"))
-
 # Set seed for reproducibility
 set.seed(123)
 
@@ -23,7 +20,8 @@ med_df <- data.frame(
   pnr = sprintf("%03d", 1:200),
   # ID variable
   eksd = as.Date(sample(
-    seq(as.Date("1995-01-01"), as.Date("2014-12-31"), by = "day"), 1000, replace = TRUE
+    seq(as.Date("1995-01-01"), as.Date("2014-12-31"), by = "day"), 1000,
+    replace = TRUE
   )),
   # Date of purchase
   apk = sample(1:3, 1000, replace = TRUE),
@@ -63,7 +61,8 @@ med_a10_df <- data.frame(
   pnr = sprintf("%03d", 1:50),
   # ID variable
   eksd = as.Date(sample(
-    seq(as.Date("1995-01-01"), as.Date("2014-12-31"), by = "day"), 1000, replace = TRUE
+    seq(as.Date("1995-01-01"), as.Date("2014-12-31"), by = "day"), 1000,
+    replace = TRUE
   )),
   # Date of purchase
   apk = sample(1:3, 1000, replace = TRUE),
@@ -100,10 +99,11 @@ med_a10_df <- data.frame(
 
 # Hardcode half of purchases to be metformin, Liraglutide or semaglutide:
 
-med_a10_df[sample(nrow(med_a10_df), nrow(med_a10_df) / 2),]$ATC <-
+med_a10_df[sample(nrow(med_a10_df), nrow(med_a10_df) / 2), ]$ATC <-
   sample(c("A10BA02", "A10BJ02", "A10BJ06"),
-         nrow(med_a10_df) / 2,
-         replace = TRUE)
+    nrow(med_a10_df) / 2,
+    replace = TRUE
+  )
 
 generateDrugName <- function(atc) {
   # You can implement your own logic to generate drug names based on ATC codes
@@ -123,8 +123,10 @@ replaceDrugNames <- function(data) {
   }
 
   # Define replacement mappings for ATC codes
-  replacement_mappings <- list("A10BJ02" = "Saxenda",
-                               "A10BJ06" = "Wegovy Flextouch")
+  replacement_mappings <- list(
+    "A10BJ02" = "Saxenda",
+    "A10BJ06" = "Wegovy Flextouch"
+  )
 
   # Iterate through rows and make replacements
   for (i in 1:nrow(data)) {
@@ -149,19 +151,23 @@ med_df <- rbind(med_df, med_a10_df)
 setDT(med_df)
 
 # Handcode a few false-positive cases with purchases of metformin:
-med_df[pnr %in% c(sprintf("%03d", 180:190)), `:=` (
+med_df[pnr %in% c(sprintf("%03d", 180:190)), `:=`(
   indo = sample(c("0000092", "0000276", "0000781"), 55, replace = TRUE),
   ATC = "A10BA02",
   drugname = "Metformin"
 )]
 
 # Handcode a few false-positive cases with purchases of Saxenda:
-med_df[pnr %in% c(sprintf("%03d", 190:195)), `:=` (ATC = "A10BJ02",
-                                                   drugname = "Saxenda")]
+med_df[pnr %in% c(sprintf("%03d", 190:195)), `:=`(
+  ATC = "A10BJ02",
+  drugname = "Saxenda"
+)]
 
 # Handcode a few false-positive cases with purchases of Wegovy:
-med_df[pnr %in% c(sprintf("%03d", 195:200)), `:=` (ATC = "A10BJ06",
-                                                   drugname = "Wegovy Flextouch")]
+med_df[pnr %in% c(sprintf("%03d", 195:200)), `:=`(
+  ATC = "A10BJ06",
+  drugname = "Wegovy Flextouch"
+)]
 
 
 
@@ -180,10 +186,94 @@ med_df[pnr %in% c(sprintf("%03d", 195:200)), `:=` (ATC = "A10BJ06",
 
 # Health Service data -----------------------------------------------------
 
+#' Create synthetic health insurance data
+#'
+#' @param num_samples Number of samples to create
+#'
+#' @return Data.frame with columns pnr, BARNMAK, SPECIALE, and HONUGE
+#'
+#' @examples
+#' create_test_hi_df(num_samples = 100)
+create_test_hi_df <- function(num_samples) {
+  data.frame(
+    # pnr: patientID (chr)
+    # random values from 001-100
+    pnr = sprintf("%03d", sample(1:100, num_samples, replace = TRUE)),
+
+    # BARNMAK: service performed on patient' child or not (binary)
+    # 1 = child, 0 = not, 5% are 1's
+    BARNMAK = sample(c(0, 1), num_samples,
+      replace = TRUE,
+      prob = c(0.95, 0.05)
+    ),
+
+    # SPECIALE: service code (6-digit int)
+    # 50% random samples between 100000 and 600000
+    # 50% random samples from 540000 to 549999
+    SPECIALE = ifelse(
+      # repeat 0 and 1 num_samples times and randomise them
+      sample(rep(c(0, 1), length.out = num_samples)),
+      # sample 100000:600000 for all 1's
+      sample(100000:600000, num_samples, replace = TRUE),
+      # sample 540000:549999 for all 0's
+      sample(540000:549999, num_samples, replace = TRUE)
+    ),
+
+    # HONUGE: year/week of the service being billed (4-digit chr)
+    # first and second digits are random numbers between 01-52
+    # third and fourth digits are random numbers between 00-99
+    HONUGE = sprintf(
+      "%02d%02d",
+      sample(1:52, num_samples, replace = TRUE),
+      sample(0:99, num_samples, replace = TRUE)
+    )
+  )
+}
+
 # create test health insurance df with 100 rows
 health_insurance_df <- create_test_hi_df(num_samples = 100)
 
 # Laboratory data ---------------------------------------------------------
+
+#' Create synthetic lab data
+#'
+#' @param num_samples Number of samples to create
+#'
+#' @return Data.frame with columns pnr, SAMPLINGDATE, ANALYSISCODE, and VALUE
+#'
+#' @examples
+#' create_test_lab_df(num_samples = 100)
+create_test_lab_df <- function(num_samples) {
+  data.frame(
+    # pnr: patient ID (chr)
+    # random ID's from 001-100 (even if num_samples > 100)
+    pnr = sprintf("%03d", sample(1:100, num_samples, replace = TRUE)),
+
+    # SAMPLINGDATE: date of sample (date)
+    # random dates between 1995-01-01 and 2015-12-31
+    SAMPLINGDATE = sample(
+      seq(as.Date("1995-01-01"), as.Date("2015-12-31"), by = "day"),
+      num_samples,
+      replace = TRUE
+    ),
+
+    # ANALYSISCODE: npu code of analysis type (chr)
+    # 50% is either NPU27300 or NPU03835
+    # other 50% is 'NPU'+random sample from 10000:99999
+    ANALYSISCODE = ifelse(
+      # repeat 0 and 1 num_samples times and randomise them
+      sample(rep(c(0, 1), length.out = num_samples)),
+      # sample 'NPU27300' and 'NPU03835' for all 1's
+      sample(c("NPU27300", "NPU03835"), num_samples, replace = TRUE),
+      # sample NPU + random digit between 10000:99999 for all 0's
+      paste0("NPU", sample(10000:99999, num_samples, replace = TRUE))
+    ),
+
+    # VALUE: numerical result of test (num)
+    # random decimal number between 0.1-99.9
+    VALUE = runif(num_samples, 0.1, 99.9)
+  )
+}
 
 # create test lab df with 100 rows
 lab_df <- create_test_lab_df(num_samples = 100)
