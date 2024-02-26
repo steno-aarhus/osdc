6fb10838377187cfe0c3b15bc082902972988b88
Luke W. Johnston <lwjohnst@gmail.com>
2024-02-26 15:32:39 +0100

    build: suppress CRAN note by assigning "data mask" .data pronoun as a variable

--- R/osdc-package.R
+++ R/osdc-package.R
@@ -5,3 +5,8 @@
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
+
+# Allows for using tidyverse functionality without triggering CRAN NOTES,
+# since CRAN doesn't know that packages like dplyr use NSE.
+# For more details, see https://rlang.r-lib.org/reference/dot-data.html#where-does-data-live-
+utils::globalVariables(".data")
