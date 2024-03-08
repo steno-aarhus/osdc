d08147ed6e97987cf569e08489caa8ba50e94442
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-08 12:21:33 +0100

    feat: internal function to get the required variables for a register.
    Closes #16

--- R/get-variables.R
+++ R/get-variables.R
@@ -2,10 +2,31 @@
 #' Get a list of the registers' abbreviations.
 #'
 #' @return A character string.
-#' @export
+#' @keywords internal
 #'
 #' @examples
 #' get_register_abbrev()
 get_register_abbrev <- function() {
-  unique(required_variables$register_abbrev)
+  unique(variable_description$register_abbrev)
+}
+
+#' Get a list of required variables from a specific register.
+#'
+#' @param register The abbreviation of the register name. See list of
+#'   abbreviations in [get_register_abbrev()].
+#'
+#' @return A character vector of variable names.
+#' @keywords internal
+#'
+#' @examples
+#' get_required_variables("bef")
+get_required_variables <- function(register) {
+  if (!checkmate::test_scalar(register)) {
+    cli::cli_abort("You are giving too many registers, please give only one.")
+  }
+  checkmate::assert_choice(register, get_register_abbrev())
+  register <- rlang::arg_match(register, get_register_abbrev())
+  variable_description |>
+    dplyr::filter(.data$register_abbrev == register) |>
+    dplyr::pull(.data$variable_name)
 }
