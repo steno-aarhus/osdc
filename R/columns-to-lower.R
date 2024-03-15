ba66e985551d2b0d54300cbfbb4af41c43092502
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-15 11:59:45 +0100

    feat: add function to convert to lowercase.
    Closes #47

---
+++ R/columns-to-lower.R
@@ -0,0 +1,13 @@
+#' Convert column names to lower case.
+#'
+#' @param data An data frame type object.
+#'
+#' @return The same object type given.
+#' @keywords internal
+#'
+#' @examples
+#' tibble::tibble(A = 1:3, B = 4:6) |>columns_to_lower()
+columns_to_lower <- function(data) {
+  data |>
+    dplyr::rename_with(tolower)
+}
