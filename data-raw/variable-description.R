45f19f07404f0661a9bdce8e002e2101a2663898
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-15 15:47:42 +0100

    chore: update data processing script, to also save internal data

--- data-raw/variable-description.R
+++ data-raw/variable-description.R
@@ -6,11 +6,16 @@ variable_description <- here::here("data-raw/variable_description.csv") |>
   read_csv() |>
   select(
     register_name,
-    register_abbrev = raw_register_filename,
+    register_abbrev,
     variable_name,
-    years_covered,
+    start_year,
+    end_year,
     danish_description,
     english_description
   )
 
+# Save to `data/` to give users access to descriptions
 usethis::use_data(variable_description, overwrite = TRUE)
+
+# Save as internal as well to give our functions access it
+usethis::use_data(variable_description, overwrite = TRUE, internal = TRUE)
