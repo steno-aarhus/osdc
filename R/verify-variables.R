01c20ece6fd56532b9cf21b373b5ab8dea6dd915
Luke W. Johnston <lwjohnst@gmail.com>
2024-03-15 15:47:06 +0100

    docs: fix roxygen docs to pass checks

--- R/verify-variables.R
+++ R/verify-variables.R
@@ -13,19 +13,17 @@
 #' @keywords internal
 #'
 #' @examples
-#' library(tibble)
-#' library(dplyr)
 #' # TODO: Replace with simulated data.
-#' example_bef_data <- tibble(pnr = 1, koen = 1, foed_dato = 1)
-#' verify_required_variables(example_bef_data, "bef")
+#' example_bef_data <- tibble::tibble(pnr = 1, koen = 1, foed_dato = 1)
+#' osdc:::verify_required_variables(example_bef_data, "bef")
 verify_required_variables <- function(data, register) {
   checkmate::assert_choice(register, get_register_abbrev())
-  
+
   # TODO: Consider using/looking into rlang::try_fetch() to provide contextual error messages.
   expected_variables <- get_required_variables(register)
 
   actual_variables <- colnames(data)
-  
+
   checkmate::check_names(
     x = actual_variables,
     must.include = expected_variables
