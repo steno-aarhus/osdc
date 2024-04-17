dc6e97d10a4d4b24de945d3024022b077727f566
Luke W. Johnston <lwjohnst@gmail.com>
2024-04-17 21:37:28 +0200

    refactor: completely refactor simulation code since simstudy was not working

--- data-raw/simulate-data.R
+++ data-raw/simulate-data.R
@@ -1,48 +1,19 @@
-# Script to generate synthetic data for tests
+# Script to generate simulated data for tests and examples
 
 # Load required libraries
 library(tidyverse)
 library(here)
 library(lubridate)
-library(simstudy)
+library(fabricatr)
+library(rvest)
 
-read_lines(
-  "https://sundhedsdatastyrelsen.dk/-/media/sds/filer/rammer-og-retningslinjer/klassifikationer/sks-download/lukkede-klassifikationer/icd-7-klassifikation.txt?la=da",
-  n_max = 10
-) |>
-  as_tibble() |>
-  separate_wider_regex(value,
-    patterns = c(
-      v1 = "dia\\d+",
-      "  +",
-      v2 = "\\d+\\D+",
-      "  +",
-      v3 = ".*"
-    )
-  ) |>
-  mutate(across(everything(), str_trim)) |>
-  mutate(
-    v2 = str_remove(v2, "\\d+"),
-    v1 = str_remove(v1, "dia")
-  ) |>
-  pull(v1)
+# Get ICD codes -----------------------------------------------------------
 
-# Set seed for reproducibility
-set.seed(123)
-simulation_definitions_path <- fs::file_temp(ext = ".csv")
-simulation_definitions <- here("data-raw/simulation-definitions.csv") |>
-  read_csv() |>
-  mutate(variance = NA,
-         link = if_else(dist == "binary", "identity", NA)) |>
-  filter(!is.na(formula)) |>
-  select(register_abbrev, varname = variable_name, formula, variance, dist, link)
+# "https://sundhedsdatastyrelsen.dk/-/media/sds/filer/rammer-og-retningslinjer/klassifikationer/sks-download/lukkede-klassifikationer/icd-8-klassifikation.txt?la=da" |>
+#   read_lines() |>
+#   write_lines(here("data-raw/icd8-codes.txt"))
 
-simulation_definitions |>
-  select(-register_abbrev) |>
-  filter(!is.na(dist)) |>
-  write_csv(simulation_definitions_path)
-
-# Helper functions --------------------------------------------------------
+# Simulation functions -----------------------------------------------------
 
 pad_integers <- function(x, width) {
   x |>
@@ -50,36 +21,87 @@ pad_integers <- function(x, width) {
     stringr::str_pad(width = width, side = "left", pad = "0")
 }
 
-#' Create a vector of fake ATC Codes.
-#'
-#' @return A vector.
-#'
-#' @examples
-#' create_fake_atc()
-create_fake_atc <- function() {
+create_fake_icd <- function(n, date = NULL) {
+  if (is.null(date)) {
+    date <- sample(c("1993-01-01", "1995-01-01"), 1)
+  }
+
+  if (date >= "1994-01-01") {
+    create_fake_icd10(n)
+  } else {
+    create_fake_icd8(n)
+  }
+}
+
+create_fake_icd8 <- function(n) {
+  here("data-raw/icd8-codes.txt") |>
+    read_lines() |>
+    str_trim() |>
+    as_tibble() |>
+    separate_wider_delim(
+      value,
+      delim = regex("   +"),
+      names = c("icd8", "description", "unknown"),
+      too_many = "merge"
+    ) |>
+    mutate(across(everything(), str_trim)) |>
+    mutate(
+      description = str_remove(description, "\\d+"),
+      icd8 = str_remove(icd8, "dia")
+    ) |>
+    pull(icd8) |>
+    sample(size = n, replace = TRUE)
+}
+
+create_fake_icd10 <- function(n) {
+  # from: https://medinfo.dk/sks/brows.php?s_nod=6308
+  here("data-raw/icd10-codes.csv") |>
+    read_csv2(show_col_types = FALSE) |>
+    pull(icd10) |>
+    sample(size = n, replace = TRUE)
+}
+
+create_fake_atc <- function(n) {
   codeCollection::ATCKoodit |>
     tibble::as_tibble() |>
     dplyr::filter(stringr::str_length(Koodi) == 7) |>
     dplyr::pull(Koodi) |>
-    sample(1, replace = TRUE)
+    sample(n, replace = TRUE)
 }
 
-create_fake_date <- function(from = "1977-01-01", to = lubridate::today()) {
+create_fake_date <- function(n, from = "1977-01-01", to = lubridate::today()) {
   seq(as_date(from), as_date(to), by = "day") |>
-    sample(1)
+    sample(n, replace = TRUE)
 }
 
-create_long_integer <- function(length) {
-  purrr::map(1:length, ~ sample(0:9, 1)) |>
-    purrr::reduce( ~ paste(.x, .y, sep = "")) |>
+create_padded_integer <- function(n, length) {
+  purrr::map(1:length, \(ignore) sample(0:9, n, replace = TRUE)) |>
+    purrr::reduce(\(integer1, integer2) paste(integer1, integer2, sep = "")) |>
     pad_integers(width = length)
+}
+
+create_fake_npu <- function(n) {
+  stringr::str_c(
+    "NPU",
+    create_padded_integer(n, 8)
+  )
+}
+
+create_fake_hovedspeciale_ans <- function(n) {
+  "https://www.dst.dk/da/Statistik/dokumentation/Times/forebyggelsesregistret/spec" |>
+    read_html() |>
+    html_element("table") |>
+    html_table() |>
+    pull(Tekst) |>
+    sample(n, replace = TRUE)
 }
 
 create_fake_drug_name <- function(atc) {
   codeCollection::ATCKoodit |>
     tibble::as_tibble() |>
-    dplyr::filter(Koodi == atc) |>
-    dplyr::pull(en)
+    dplyr::filter(Koodi %in% atc) |>
+    dplyr::pull(en) |>
+    sample(length(atc), replace = TRUE)
 }
 
 to_wwyy <- function(x) {
@@ -90,101 +112,38 @@ to_yyyymmdd <- function(x) {
   format(lubridate::as_date(x), format = "%Y%m%d")
 }
 
+# Insert extra values ------------------------------------------------------
 
-# Fixers post-simulation --------------------------------------------------
-
-fix_senere_afkraeftet <- function(data) {
-  data |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::contains("senere_afkraeftet"),
-        ~dplyr::if_else(
-          . == 0,
-          "Ja",
-          "Nej"
-        )
-      )
-    )
-}
-
-fix_analysiscode <- function(data) {
-  data |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::contains("analysiscode"),
-        ~stringr::str_c("NPU", .)
-      )
-    )
-}
-
-# Should be 1 and 2, not 0 and 1 that is created by simstudy
-fix_koen <- function(data) {
-  data |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::contains("koen"),
-        # 2 is woman, 1 is man
-        ~. + 1
-      )
-    )
-}
-
-fix_c_diagtype <- function(data) {
-  data |>
-    dplyr::mutate(
-      dplyr::across(
-        dplyr::contains("c_diagtype"),
-        ~dplyr::if_else(
-          . == 0,
-          # Primary diagnosis
-          "A",
-          # Secondary diagnosis
-          "B"
-        )
-      )
-    )
-}
-genData(100, defRead(simulation_definitions_path)) |>
-  as_tibble()
-
-# Medication data ---------------------------------------------------------
-
-
-## Diabetes data (ID 201-250):
-
-insert_atc
-        "A10AB",
-        "A10AC",
-        "A10AD",
-        "A10AE",
-        "A10BA",
-        "A10BB",
-        "A10BD",
-        "A10BG",
-        "A10BH",
-        "A10BJ",
-        "A10BK",
-        "A10BX"
-
-# Hardcode half of purchases to be metformin, liraglutid or semaglutid:
-
-med_a10_df[sample(nrow(med_a10_df), nrow(med_a10_df) / 2), ]$ATC <-
-  sample(c("A10BA02", "A10BJ02", "A10BJ06"),
-    nrow(med_a10_df) / 2,
-    replace = TRUE
-  )
-
-replaceDrugNames <- function(data) {
-  # With 50% replace rate
-  # Define replacement mappings for ATC codes
-  list(
-    "A10BJ02" = "Saxenda",
-    "A10BJ06" = "Wegovy Flextouch"
-  )
-}
-
-false_positive <- function(proportion) {
+insertion_rate <- function(proportion) {
   runif(1) < proportion
+}
+
+insert_specific_atc <- function(data, proportion = 0.3) {
+  # c(
+  #       "A10AB",
+  #       "A10AC",
+  #       "A10AD",
+  #       "A10AE",
+  #       "A10BA",
+  #       "A10BB",
+  #       "A10BD",
+  #       "A10BG",
+  #       "A10BH",
+  #       "A10BJ",
+  #       "A10BK",
+  #       "A10BX"
+  # )
+  data |>
+    mutate(
+      across(
+        matches("^atc$"),
+        \(column) if_else(
+          runif(n()) < proportion,
+          sample(c("A10BA02", "A10BJ02", "A10BJ06"), 1),
+          column
+        )
+      )
+    )
 }
 
 # Insert a few false-positive cases with purchases of metformin:
@@ -195,48 +154,31 @@ insert_false_metformin <- function(data, proportion = 0.05) {
   data |>
     dplyr::mutate(
       atc = dplyr::if_else(
-        indo %in% c("0000092", "0000276", "0000781") & false_positive(proportion),
+        indo %in% c("0000092", "0000276", "0000781") & insertion_rate(proportion),
         "A10BA02",
         atc
       ),
       name = dplyr::if_else(
-        indo %in% c("0000092", "0000276", "0000781") & false_positive(proportion),
+        indo %in% c("0000092", "0000276", "0000781") & insertion_rate(proportion),
         "metformin",
         name
       )
     )
 }
 
-insert_wegovy <- function() {
-
-}
-
 # Insert some false positives for Wegovy and Saxenda.
-insert_drugnames <- function(x, atc, name, proportion = 0.05) {
-  tibble::tribble(
-    ~ATC, ~name,
-    "A10BJ06", "Wegovy Flextouch",
-    "A10BJ02", "Saxenda"
-  )
-  dplyr::if_else(
-    ATC == atc & false_positive(proportion),
-    name,
-    x
-  )
-}
-
-fix_speciale <- function(x) {
-  # TODO: Is this necessary?
-  # 50% random samples between 100000 and 600000
-  # 50% random samples from 540000 to 549999
-  SPECIALE = ifelse(
-    # repeat 0 and 1 num_samples times and randomise them
-    sample(rep(c(0, 1), length.out = num_samples)),
-    # sample 100000:600000 for all 1's
-    sample(100000:600000, num_samples, replace = TRUE),
-    # sample 540000:549999 for all 0's
-    sample(540000:549999, num_samples, replace = TRUE)
-  )
+insert_false_drug_names <- function(data, proportion = 0.05) {
+  if (!all(colnames(data) %in% c("atc", "name"))) {
+    return(data)
+  }
+  data |>
+    mutate(
+      name = case_when(
+        atc == "A10BJ06" & insertion_rate(proportion) ~ "Wegovy Flextouch",
+        atc == "A10BJ02" & insertion_rate(proportion) ~ "Saxenda",
+        TRUE ~ name
+      )
+    )
 }
 
 insert_analysiscode <- function(x) {
@@ -246,3 +188,53 @@ insert_analysiscode <- function(x) {
   # other 50% is NPU10000 to NPU99999
   x
 }
+
+# Generate data -----------------------------------------------------------
+
+# use the simulation definition data to simulate some data
+simulate_data <- function(data, n) {
+  N <- n
+  data$generator |>
+    as.list() |>
+    set_names(data$variable_name) |>
+    # this evaluates the character string
+    map(~ eval(parse(text = .x))) |>
+    # enframe converts a list to a tibble
+    imap(\(column, name) enframe(column, name = NULL, value = name)) |>
+    unname() |>
+    list_cbind()
+}
+
+add_fake_drug_name <- function(data) {
+  data |>
+    mutate(
+      across(
+        matches("^name$"),
+        \(x) create_fake_drug_name(atc = atc)
+      )
+    )
+}
+
+set.seed(123)
+simulation_definitions <- here("data-raw/simulation-definitions.csv") |>
+  read_csv(show_col_types = FALSE) |>
+  select(register_abbrev, variable_name, generator)
+
+simulation_definitions_list <- simulation_definitions |>
+  group_split(register_abbrev)
+
+register_data <- simulation_definitions_list |>
+  map(\(data) simulate_data(data, n = 1000)) |>
+  map(insert_specific_atc) |>
+  map(add_fake_drug_name) |>
+  map(insert_false_drug_names) |>
+  map(insert_false_metformin) |>
+  # add the register abbreviations as a name to the list
+  set_names(
+    map_chr(
+      simulation_definitions_list,
+      \(data) unique(data$register_abbrev)
+    )
+  )
+
+usethis::use_data(register_data, overwrite = TRUE)
