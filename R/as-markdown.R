53108a98423824a241c5781733e46e932b3c6ac4
Luke W. Johnston <lwjohnst@gmail.com>
2024-05-02 15:29:47 +0200

    feat: helper functions to insert data into Markdown vignettes

--- R/as-markdown.R
+++ R/as-markdown.R
@@ -2,10 +2,13 @@
 #'
 #' @param caption Caption to add to the table.
 #'
-#' @return A [tibble] object.
+#' @return A character vector as a Markdown table.
 #' @keywords internal
 #'
 registers_as_md_table <- function(caption = NULL) {
+  rlang::check_installed("glue")
+  rlang::check_installed("knitr")
+
   variable_description |>
     dplyr::select(
       .data$register_name,
@@ -24,5 +27,59 @@ registers_as_md_table <- function(caption = NULL) {
       "Abbreviation" = .data$register_abbrev,
       "Years" = .data$years
     ) |>
+    knitr::kable(caption = caption)
+}
+
+#' Convert the register name into text to use in a Markdown header.
+#'
+#' @param register The abbreviation of the register name.
+#'
+#' @return A character vector.
+#' @keywords internal
+#'
+register_as_md_header <- function(register) {
+  rlang::check_installed("glue")
+
+  variable_description |>
+    dplyr::distinct(.data$register_name, .data$register_abbrev) |>
+    dplyr::filter(.data$register_abbrev == register) |>
+    glue::glue_data(
+      "`{register_abbrev}`: {register_name}"
+    )
+}
+
+#' Convert the fake register data into a Markdown table.
+#'
+#' @param register The abbreviation of the register name.
+#' @param caption A caption to add to the table.
+#'
+#' @return A character vector as a Markdown table.
+#' @keywords internal
+#'
+register_data_as_md_table <- function(register, caption = NULL) {
+  rlang::check_installed("glue")
+  rlang::check_installed("knitr")
+
+  register_data[[register]] |>
+    head(4) |>
+    knitr::kable(caption = caption)
+}
+
+#' Converts the variables for a register into a Markdown table.
+#'
+#' @inheritParams register_data_as_md_table
+#'
+#' @return A character vector as a Markdown table.
+#' @keywords internal
+#'
+variables_as_md_table <- function(register, caption = NULL) {
+  rlang::check_installed("glue")
+  rlang::check_installed("knitr")
+  rlang::check_installed("stringr")
+
+  variable_description |>
+    dplyr::filter(.data$register_abbrev == register) |>
+    dplyr::select(.data$variable_name, .data$english_description) |>
+    dplyr::mutate(english_description = stringr::str_to_sentence(.data$english_description)) |>
     knitr::kable(caption = caption)
 }
