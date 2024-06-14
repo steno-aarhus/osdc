f23609a080df097f73ba6f2dfd164ce1a92ab4a5
Luke W. Johnston <lwjohnst@gmail.com>
2024-06-14 12:59:49 +0200

    test: :white_check_mark: add additional tests for the filtering to check duplicate dates

--- tests/testthat/test-include-hba1c.R
+++ tests/testthat/test-include-hba1c.R
@@ -1,26 +1,38 @@
 lab_forsker <- tibble::tribble(
   ~patient_cpr, ~samplingdate, ~analysiscode, ~value,
+  "498718589800", "20230101", "NPU27300", 49,
+  "498718589801", "20230101", "NPU03835", 6.6,
+  "498718589802", "20230101", "NPU03835", 6.3,
   "498718589803", "20230101", "NPU27300", 47,
-  "498718589803", "20210101", "NPU27300", 49,
-  "498718589804", "20220101", "NPU27300", 47,
   # Duplicate patient_cpr but with the old units.
+  "498718589803", "20210101", "NPU27300", 49,
   "498718589803", "20220101", "NPU03835", 6.5,
-  "498718589805", "20220101", "NPU03835", 6.5,
-  "498718589806", "20220101", "NPU03835", 6.3,
-  "498718589807", "20220101", "NPU00000", 100,
-  "498718589808", "20220101", "NPU00000", 5
+  # Duplicate patient_cpr when old and new units are the same date.
+  "498718589805", "20000101", "NPU03835", 6.5,
+  "498718589805", "20000101", "NPU27300", 49,
+  # Duplicate but with old below threshold and new above it.
+  "498718589806", "20000101", "NPU03835", 6.3,
+  "498718589806", "20000101", "NPU27300", 49,
+  # Duplicate but with new below threshold and old above it.
+  "498718589807", "20200101", "NPU03835", 6.6,
+  "498718589807", "20200101", "NPU27300", 47,
+  "498718589808", "20220101", "NPU00000", 100,
+  "498718589809", "20220101", "NPU00000", 5
 )
 
 expected <- tibble::tribble(
   ~pnr, ~date, ~included_hba1c,
+  "498718589800", "20230101", TRUE,
+  "498718589801", "20230101", TRUE,
   "498718589803", "20210101", TRUE,
   "498718589803", "20220101", TRUE,
-  "498718589805", "20220101", TRUE
+  "498718589805", "20000101", TRUE,
+  "498718589806", "20000101", TRUE,
+  "498718589807", "20200101", TRUE
 )
 
 test_that("dataset needs expected variables", {
-  actual <- lab_forsker |>
-    select(-patient_cpr)
+  actual <- lab_forsker
   expect_error(include_hba1c(actual))
 })
 
