fcf17b17f676f98abe7cc42600a4cdd44a13b201
Luke W. Johnston <lwjohnst@gmail.com>
2024-06-14 15:41:49 +0200

    fix: this function should have given an error,  so fixed so it does give an error

--- R/verify-variables.R
+++ R/verify-variables.R
@@ -8,24 +8,27 @@
 #' @param data The dataset to check.
 #' @inheritParams get_required_variables
 #'
-#' @return Either TRUE if the verification passes, or a character string if
-#'   there is an error.
+#' @return Either TRUE if the verification passes, or an error.
 #' @keywords internal
 #'
 #' @examples
 #' # TODO: Replace with simulated data.
-#' example_bef_data <- tibble::tibble(pnr = 1, koen = 1, foed_dato = 1)
-#' osdc:::verify_required_variables(example_bef_data, "bef")
-verify_required_variables <- function(data, register) {
+#' verify_required_variables(register_data$bef, "bef")
+#' verify_required_variables(register_data$bef, "lpr_adm")
+verify_required_variables <- function(data, register, call = rlang::caller_env()) {
   checkmate::assert_choice(register, get_register_abbrev())
+  expected_variables <- sort(get_required_variables(register))
+  actual_variables <- sort(colnames(data))
 
-  # TODO: Consider using/looking into rlang::try_fetch() to provide contextual error messages.
-  expected_variables <- get_required_variables(register)
-
-  actual_variables <- colnames(data)
-
-  checkmate::check_names(
+  if (!checkmate::test_names(
     x = actual_variables,
     must.include = expected_variables
-  )
+  )) {
+    cli::cli_abort(c(
+      "This function needs specific variables from the {.val {register}} register.",
+      "i" = "Variables required: {.val {expected_variables}}",
+      "x" = "Variables found: {.val {actual_variables}}"
+    ))
+  }
+  return(invisible(TRUE))
 }
