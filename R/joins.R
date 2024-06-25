6484724de45a808d975b37f0e8f12b792300ae9c
Anders Aasted Isaksen <ANDAAS@onerm.dk>
2024-06-25 12:36:39 +0200

    Changed to inner join and added non-joinable rows to simulate bad data situation.

--- R/joins.R
+++ R/joins.R
@@ -9,13 +9,11 @@
 #' @examples
 #' register_data$lpr_diag |>
 #'   join_lpr2(register_data$lpr_adm)
-join_lpr2 <- function(lpr_diag, lpr_adm) {
-  verify_required_variables(lpr_diag, "lpr_diag")
+join_lpr2 <- function(lpr_adm, lpr_diag) {
   verify_required_variables(lpr_adm, "lpr_adm")
-  dplyr::full_join(
-    column_names_to_lower(lpr_adm),
-    column_names_to_lower(lpr_diag),
-    by = "recnum"
-  )
+  verify_required_variables(lpr_diag, "lpr_diag")
+  dplyr::inner_join(column_names_to_lower(lpr_adm),
+                    column_names_to_lower(lpr_diag),
+                    by = "recnum")
 }
 
--- tests/testthat/test-joins.R
+++ tests/testthat/test-joins.R
@@ -1,29 +1,29 @@
 actual_lpr_diag <- tibble::tibble(
-  recnum = rep(1:4, each = 2),
-  c_diag = 1:8,
-  c_diagtype = rep(c("A", "B"), 4)
+  recnum = rep(1:6, each = 2),
+  c_diag = 1:12,
+  c_diagtype = rep(c("A", "B"), 6)
 )
 
 actual_lpr_adm <- tibble::tibble(
-  pnr = rep(1:2, 2),
-  recnum = 1:4,
-  c_spec = 1:4,
-  d_inddto = c("20230101", "20220101", "20210101", "20200101"),
+  pnr = rep(1:2, 3),
+  recnum = 2:7,
+  c_spec = 1:6,
+  d_inddto = c("20230101", "20220101", "20210101", "20200101", "20190101", "20180101"),
 )
 
 expected_lpr2 <- tibble::tibble(
-  pnr = rep(1:2, 2, each = 2),
-  recnum = rep(1:4, each = 2),
-  c_spec = rep(1:4, each = 2),
-  d_inddto = rep(c("20230101", "20220101", "20210101", "20200101"), each = 2),
-  c_diag = 1:8,
-  c_diagtype = rep(c("A", "B"), 4)
+  pnr = rep(1:2, length.out = 10, each = 2),
+  recnum = rep(2:6, length.out = 10, each = 2),
+  c_spec = rep(1:5, length.out = 10, each = 2),
+  d_inddto = rep(c("20230101", "20220101", "20210101", "20200101", "20190101"), each = 2),
+  c_diag = 3:12,
+  c_diagtype = rep(c("A", "B"), length.out = 10)
 )
 
 test_that("joining LPR2 correctly", {
   actual <- join_lpr2(
-    actual_lpr_diag,
-    actual_lpr_adm
+    actual_lpr_adm,
+    actual_lpr_diag
   )
 
   expect_equal(actual, expected_lpr2)
