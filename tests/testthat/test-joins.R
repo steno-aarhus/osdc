4b0f391d443ce05d688a136544e697b62c31bd93
Luke W. Johnston <lwjohnst@gmail.com>
2024-06-25 16:50:54 +0200

    test: revise to test if other data sources also calculate correctly

--- tests/testthat/test-joins.R
+++ tests/testthat/test-joins.R
@@ -29,30 +29,65 @@ test_that("joining LPR2 correctly", {
   expect_equal(actual, expected_lpr2)
 })
 
+test_that("lpr_adm and lpr_diag must be in correct arg position", {
+  expect_error(join_lpr2(
+    actual_lpr_diag,
+    actual_lpr_adm
+  ))
+})
+
 test_that("joining works for DuckDB Database", {
-  actual <- arrow::to_duckdb(actual_lpr_diag) |>
-    join_lpr2(arrow::to_duckdb(actual_lpr_adm))
+  actual <- arrow::to_duckdb(actual_lpr_adm) |>
+    join_lpr2(arrow::to_duckdb(actual_lpr_diag))
+
+  actual_rows <- actual |>
+    dplyr::count() |>
+    dplyr::pull(n) |>
+    as.integer()
 
   expect_contains(class(actual), "tbl_duckdb_connection")
+  expect_identical(colnames(actual), colnames(expected_lpr2))
+  expect_identical(actual_rows, nrow(expected_lpr2))
 })
 
 test_that("joining works for Arrow Tables (from Parquet)", {
-  actual <- arrow::as_arrow_table(actual_lpr_diag) |>
-    join_lpr2(arrow::as_arrow_table(actual_lpr_adm))
+  actual <- arrow::as_arrow_table(actual_lpr_adm) |>
+    join_lpr2(arrow::as_arrow_table(actual_lpr_diag))
+
+  actual_rows <- actual |>
+    dplyr::count() |>
+    dplyr::pull(n) |>
+    as.integer()
 
   expect_contains(class(actual), "arrow_dplyr_query")
+  expect_identical(names(actual), colnames(expected_lpr2))
+  expect_identical(actual_rows, nrow(expected_lpr2))
 })
 
 test_that("joining works for data.frame", {
-  actual <- as.data.frame(actual_lpr_diag) |>
-    join_lpr2(as.data.frame(actual_lpr_adm))
+  actual <- as.data.frame(actual_lpr_adm) |>
+    join_lpr2(as.data.frame(actual_lpr_diag))
+
+  actual_rows <- actual |>
+    dplyr::count() |>
+    dplyr::pull(n) |>
+    as.integer()
 
   expect_contains(class(actual), "data.frame")
+  expect_identical(names(actual), colnames(expected_lpr2))
+  expect_identical(actual_rows, nrow(expected_lpr2))
 })
 
 test_that("joining works for data.table", {
-  actual <- data.table::as.data.table(actual_lpr_diag) |>
-    join_lpr2(data.table::as.data.table(actual_lpr_adm))
+  actual <- data.table::as.data.table(actual_lpr_adm) |>
+    join_lpr2(data.table::as.data.table(actual_lpr_diag))
+
+  actual_rows <- actual |>
+    dplyr::count() |>
+    dplyr::pull(n) |>
+    as.integer()
 
   expect_contains(class(actual), "data.table")
+  expect_identical(colnames(actual), colnames(expected_lpr2))
+  expect_identical(actual_rows, nrow(expected_lpr2))
 })
