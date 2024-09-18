49e3529bb9747324aee201e80ee66d03ac48ad02
Luke W. Johnston <lwjohnst@gmail.com>
2024-09-18 14:29:15 +0200

    fix: :bug: when verifying, the variables should be converted to lower case

--- R/verify-variables.R
+++ R/verify-variables.R
@@ -22,7 +22,7 @@
 verify_required_variables <- function(data, register, call = rlang::caller_env()) {
   checkmate::assert_choice(register, get_register_abbrev())
   expected_variables <- sort(get_required_variables(register))
-  actual_variables <- sort(colnames(data))
+  actual_variables <- tolower(sort(colnames(data)))
 
   if (!checkmate::test_names(
     x = actual_variables,
