bdcc0048bcbb0c2dc122004c42c128a7766c0ad4
Luke W. Johnston <lwjohnst@gmail.com>
2024-09-18 14:29:59 +0200

    tests: duckplyr outputs a lot of messages, remove them in tests

--- tests/testthat/helper-options.R
+++ tests/testthat/helper-options.R
@@ -1,3 +1,8 @@
 # There is a warning when using Arrow about using pull. It says to set
 # this option.
-options(arrow.pull_as_vector = TRUE)
+options(
+  arrow.pull_as_vector = TRUE,
+  duckdb.materialize_message = FALSE
+)
+Sys.setenv(DUCKPLYR_FALLBACK_COLLECT = 0)
+Sys.setenv(DUCKPLYR_FALLBACK_VERBOSE = FALSE)
