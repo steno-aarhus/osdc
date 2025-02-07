431099a303eb53c0dc92c5aa11bdd46a44e5858f
Luke W. Johnston <lwjohnst@gmail.com>
2025-02-07 11:23:15 +0100

    test: :white_check_mark: simplify tests while checking realistic start/end dates

--- tests/testthat/test-yyww-to-yyyymmdd.R
+++ tests/testthat/test-yyww-to-yyyymmdd.R
@@ -1,28 +1,12 @@
-test_that("conversion works when 01-01 is Monday in week 1", {
-  expect_equal(yyww_to_yyyymmdd("2439"), lubridate::ymd("2024-09-23"))
+test_that("date correctly converts to real dates for end and start of year", {
+  # Using real dates from https://en.wikipedia.org/wiki/ISO_week_date
+  expected <- lubridate::ymd("2007-01-01", "2009-12-28", "2008-12-22")
+  actual <- yyww_to_yyyymmdd(c("0701", "0953", "0852"))
+  expect_equal(expected, actual)
 })
 
-test_that("conversion works when 01-01 is Friday in week 52 of the prior year", {
-  expect_equal(yyww_to_yyyymmdd("9307"), lubridate::ymd("1993-02-15"))
-})
-
-test_that("conversion works for week 53", {
-  expect_equal(yyww_to_yyyymmdd("1853"), lubridate::ymd("2018-12-31"))
-})
-
-test_that("conversion works for zero-padded year and week (i.e., numbers < 10)", {
-  expect_equal(yyww_to_yyyymmdd("0107"), lubridate::ymd("2001-02-12"))
-})
-
-test_that("conversion works for numeric one digit year", {
+test_that("conversion works when date is numeric", {
   # This could happen if the input was "0107" and has been converted to numeric
   expect_equal(yyww_to_yyyymmdd(107), lubridate::ymd("2001-02-12"))
-})
-
-test_that("conversion works for numeric year 2000", {
-  expect_equal(yyww_to_yyyymmdd(0007), lubridate::ymd("2000-02-14"))
-})
-
-test_that("conversion works for multiple inputs", {
-  expect_equal(yyww_to_yyyymmdd(c("0107", "2439")), lubridate::ymd("2001-02-12", "2024-09-23"))
+  expect_equal(yyww_to_yyyymmdd(7), lubridate::ymd("2000-02-14"))
 })
