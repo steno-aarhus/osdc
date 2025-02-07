5564d2a028d64c5a3ced68b58b1f0ee4812624a3
Luke W. Johnston <lwjohnst@gmail.com>
2025-02-07 11:52:39 +0100

    fix: :bug: this function needs to be exported so Arrow can find it

--- R/yyww-to-yyyymmdd.R
+++ R/yyww-to-yyyymmdd.R
@@ -3,15 +3,16 @@
 #' Since the exact date isn't given in the input, this function will set the
 #' date to Monday of the week. As a precaution, a leading zero is added if it
 #' has been removed. This can e.g., happen if the input was "0107" and has been
-#' converted to a numeric 107.
+#' converted to a numeric 107. We need to export this function so that it can
+#' be found when using Arrow to process the data.
 #'
 #' @param yyww Character(s) of the format YYWW.
 #'
 #' @returns Date(s) in the format YYYY-MM-DD.
-#'
-#' @keywords internal
+#' @export
 #'
 #' @examples
+#' \dontrun{
 #' yyww_to_yyyymmdd("0101")
 #' yyww_to_yyyymmdd("2402")
 #' yyww_to_yyyymmdd("2453")
@@ -19,6 +20,7 @@
 #' yyww_to_yyyymmdd(c("0100", "0353"))
 #' yyww_to_yyyymmdd(c("0953", "0701"))
 #' yyww_to_yyyymmdd(953)
+#' }
 yyww_to_yyyymmdd <- function(yyww) {
   if (is.numeric(yyww)) {
     # Ensure input is zero-padded to length 4
