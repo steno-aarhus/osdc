0eb3fac2a35f22c5ff58212e80a0ef3889b1d96a
Luke W. Johnston <lwjohnst@gmail.com>
2025-03-12 12:31:58 +0100

    refactor: :recycle: move algorithm data into its own R code
    Closes #200

--- R/osdc-package.R
+++ R/osdc-package.R
@@ -21,22 +21,3 @@ utils::globalVariables(".data")
 #' @format
 #' Is a list with several registers.
 "register_data"
-
-#' Data frame of the logic for the OSDC algorithm
-#'
-#' This data frame contains the logic details of the algorithm for specific
-#' inclusion and exclusion criteria.
-#'
-#' @format
-#' Is a [tibble::tibble()] with two columns:
-#'
-#' \describe{
-#'  \item{register}{Optional. The register used for this criteria.}
-#'  \item{name}{The inclusion or exclusion criteria name.}
-#'  \item{title}{The title to use when displaying the algorithmic logic in tables.}
-#'  \item{logic}{The logic for the criteria.}
-#'  \item{comments}{Some additional comments on the criteria.}
-#' }
-#' @seealso See the `vignette("algorithm")` and [algorithm] for the logic used
-#'   to filter these patients.
-"algorithm"
