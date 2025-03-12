427545128f2422287271f0fc22f0cbb92c6f7727
Signe Kirk Brødbæk <signekb@clin.au.dk>
2025-03-12 12:32:33 +0100

    test: :white_check_mark: update arrow test to compute arrow query
    Without `dplyr::compute()`, `actual` is a query and not a table. This is also why `colnames()` didn't work in the same test.

--- tests/testthat/test-include-hba1c.R
+++ tests/testthat/test-include-hba1c.R
@@ -76,10 +76,7 @@ test_that("verification works for Arrow Tables (from Parquet)", {
   skip_on_cran()
   skip_if_not_installed("arrow")
   actual <- arrow::as_arrow_table(lab_forsker) |>
-    include_hba1c() |>
-    # TODO: Arrow doesn't like the `row_number()` function, find a fix?
-    # Ignoring the warning for now, low priority.
-    suppressWarnings()
+    include_hba1c() |> dplyr::compute()
 
   actual_rows <- actual |>
     dplyr::count() |>
