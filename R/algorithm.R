13ea4fe9be9238481e8fe9c74676d98b32cf012a
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-06-16 13:42:50 +0200

    fix: :bug: `barnmak` logic in`podiatrist_services`
    Also updated the meaning of 0 and 1, respectively.

--- R/algorithm.R
+++ R/algorithm.R
@@ -113,9 +113,8 @@ algorithm <- function() {
     podiatrist_services = list(
       register = NA,
       title = "Podiatrist services",
-      logic = "speciale =~ '^54' AND barnmak != 0",
-      # TODO: Explain what barnmark 0 means
-      comments = "`barnmak` means the services were provided to a child of the individual."
+      logic = "speciale =~ '^54' AND barnmak == 0",
+      comments = "`barnmak` means the services were provided to a child of the individual. When barnmak == 0, the PNR belongs to the recipient of the service. When barnmak == 1, the PNR belongs to the child of the individual."
     ),
     no_potential_pcos = list(
       register = NA,
