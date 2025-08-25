9e17aa7b09a25a064de41589b1db8a57761c870f
Luke W. Johnston <lwjohnst@gmail.com>
2025-08-25 16:31:18 +0200

    test: :white_check_mark: check for output of trues for GLD purchases

--- tests/testthat/test-include-gld-purchases.R
+++ tests/testthat/test-include-gld-purchases.R
@@ -51,7 +51,12 @@ test_that("dataset needs expected variables", {
   expect_error(include_gld_purchases(actual))
 })
 
-test_that("those with inclusion are kept", {
+test_that("there's at least some 'cases' in the simulated data", {
   actual <- include_gld_purchases(lmdb)
-  expect_equal(actual, expected)
+  expect_equal(nrow(dplyr::count(actual, has_two_thirds_insulin)), 2)
+  expect_equal(nrow(dplyr::count(actual, has_only_insulin_purchases)), 2)
+  expect_equal(
+    nrow(dplyr::count(actual, has_insulin_purchases_within_180_days)),
+    2
+  )
 })
