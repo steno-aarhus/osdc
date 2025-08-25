ec1e50fe490365c0af815301881c4a26b8e60f8d
Luke W. Johnston <lwjohnst@gmail.com>
2025-08-25 17:57:49 +0200

    test: :fire: these tests shouldn't have been there in the first place...

--- tests/testthat/test-include-gld-purchases.R
+++ tests/testthat/test-include-gld-purchases.R
@@ -1,21 +1,6 @@
 lmdb <- simulate_registers("lmdb", 1000)[[1]]
 
-test_that("dataset needs expected variables", {
-  actual <- lmdb[-2]
-  expect_error(include_gld_purchases(actual))
-})
-
 test_that("creates a data.frame output", {
   actual <- include_gld_purchases(lmdb)
   expect_contains(class(actual), "data.frame")
-})
-
-test_that("there's at least some 'cases' in the simulated data", {
-  actual <- include_gld_purchases(lmdb)
-  expect_equal(nrow(dplyr::count(actual, has_two_thirds_insulin)), 2)
-  expect_equal(nrow(dplyr::count(actual, has_only_insulin_purchases)), 2)
-  expect_equal(
-    nrow(dplyr::count(actual, has_insulin_purchases_within_180_days)),
-    2
-  )
 })
