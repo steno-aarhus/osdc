3a8c9c0e7a1e015ee62269f29ecdaf40158538f9
Luke W. Johnston <lwjohnst@gmail.com>
2025-08-25 12:58:26 +0200

    test: :white_check_mark: simplify tests, moving logic tests into `classify_diabetes()`

--- tests/testthat/test-include-gld-purchases.R
+++ tests/testthat/test-include-gld-purchases.R
@@ -1,60 +1,16 @@
-constants <- tibble::tibble(
-  pnr = "2132131231",
-  eksd = "20210101",
-  volume = 1.1,
-  apk = 1.1,
-  indo = "324314324"
-)
-
-lmdb <- tibble::tribble(
-  ~atc,
-  "A10A23",
-  "A10",
-  "A10C",
-  "A10B",
-  "A10AE56",
-  "A10B02",
-  "A11",
-  "A21",
-  "B10A10",
-) |>
-  dplyr::bind_cols(constants)
-
-expected <- tibble::tribble(
-  ~atc, ~is_insulin_gld_code, ~is_non_insulin_gld_code,
-  "A10A23", TRUE, FALSE,
-  "A10", FALSE, FALSE,
-  # This ATC doesn't exist, but is added to be certain
-  # the function correctly identifies the drug classes.
-  "A10C", FALSE, FALSE,
-  "A10B", FALSE, TRUE,
-  "A10AE56", FALSE, TRUE,
-  "A10B02", FALSE, TRUE
-) |>
-  dplyr::bind_cols(constants) |>
-  dplyr::rename(date = eksd, indication_code = indo) |>
-  dplyr::mutate(
-    contained_doses = volume * apk,
-    has_gld_purchases = TRUE,
-  ) |>
-  dplyr::select(-volume, -apk) |>
-  dplyr::relocate(
-    pnr,
-    date,
-    atc,
-    contained_doses,
-    has_gld_purchases,
-    indication_code,
-    is_insulin_gld_code,
-    is_non_insulin_gld_code
-  )
+lmdb <- simulate_registers("lmdb", 1000)[[1]]
 
 test_that("dataset needs expected variables", {
   actual <- lmdb[-2]
   expect_error(include_gld_purchases(actual))
 })
 
-test_that("those with inclusion are kept", {
+test_that("has at least some 'cases' from simulated data", {
   actual <- include_gld_purchases(lmdb)
-  expect_equal(actual, expected)
+  expect_equal(nrow(dplyr::count(actual, has_two_thirds_insulin)), 2)
+  expect_equal(nrow(dplyr::count(actual, has_only_insulin_purchases)), 2)
+  expect_equal(
+    nrow(dplyr::count(actual, has_insulin_purchases_within_180_days)),
+    2
+  )
 })
