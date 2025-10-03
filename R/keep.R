a84620ed1f6132846f2957d044993cfebcb57906
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-10-03 09:47:24 +0200

    docs: :memo: add details to function doc
    About when/where it is applied in `classify_diabetes()`

--- R/keep.R
+++ R/keep.R
@@ -240,7 +240,13 @@ yyww_to_yyyymmdd <- function(yyww) {
 #' Keep two earliest events per PNR
 #'
 #' Since the classification date is based on the second instance of
-#' an inclusion criteria, we only need to keep the earliest two events per PNR.
+#' an inclusion criteria, we only need to keep the earliest two events per PNR
+#' per inclusion "stream".
+#'
+#' This function is applied to each "stream", `diabetes_diagnoses`,
+#' `podiatrist_services`, and `gld_hba1c_after_drop_steps`, in  the
+#' `classify_diabetes()` function after the keep and drop steps, right before
+#'  they are joined.
 #'
 #' @param data Data including at least a date and pnr column.
 #'
