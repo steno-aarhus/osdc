bd3b61d35cc38201f77daa27ed285e77f3fc2066
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-11-18 15:39:33 +0100

    test: :white_check_mark: update expected for podiatrist services to include helper variable

--- tests/testthat/test-keep.R
+++ tests/testthat/test-keep.R
@@ -124,11 +124,11 @@ sssy <- tibble::tribble(
 )
 
 expected <- tibble::tribble(
-  ~pnr, ~date,
-  2000000000, lubridate::ymd("1992-02-10"),
-  3000000000, lubridate::ymd("2007-12-24"),
-  3000000000, lubridate::ymd("2024-07-22"),
-  3000000000, lubridate::ymd("2008-07-21")
+  ~pnr, ~date, ~has_podiatrist_service,
+  2000000000, lubridate::ymd("1992-02-10"), TRUE,
+  3000000000, lubridate::ymd("2007-12-24"), TRUE,
+  3000000000, lubridate::ymd("2024-07-22"), TRUE,
+  3000000000, lubridate::ymd("2008-07-21"), TRUE
 )
 
 test_that("sysi needs expected variables", {
