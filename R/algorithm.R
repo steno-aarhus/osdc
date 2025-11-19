a010d07c6e72e4f36a01284d00b5255ace4ad81f
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-11-19 12:42:01 +0100

    refactor: :recycle: add logical inclusion vars to `has_t1d` logic
    Including that the classification needs two or more of these helper variables to be True.

--- R/algorithm.R
+++ R/algorithm.R
@@ -159,7 +159,7 @@ algorithm <- function() {
     has_t1d = list(
       register = NA,
       title = "Classifying type 1 diabetes status",
-      logic = "(has_only_insulin_purchases & has_any_t1d_primary_diagnosis) | (!has_only_insulin_purchases & has_majority_t1d_diagnoses & has_two_thirds_insulin & has_insulin_purchases_within_180_days)",
+      logic = "(has_diabetes_diagnosis OR has_podiatrist_service OR has_gld_purchase OR has_hba1c_over_threshold)>=2 AND (has_only_insulin_purchases & has_any_t1d_primary_diagnosis) OR (!has_only_insulin_purchases & has_majority_t1d_diagnoses & has_two_thirds_insulin & has_insulin_purchases_within_180_days)",
       comments = "The final classification for type 1 diabetes. Depends on all the previous steps to create these intermediate logical variables."
     ),
     has_any_t1d_primary_diagnosis = list(
