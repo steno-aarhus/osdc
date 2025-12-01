6650da4bac40c4a1efea131b517c1188938718bd
Luke W. Johnston <lwjohnst@gmail.com>
2025-12-01 09:45:36 +0100

    test: :bug: not sure if this will fix the check error

--- tests/testthat/test-classify-non-cases.R
+++ tests/testthat/test-classify-non-cases.R
@@ -1,18 +1,23 @@
 test_that("expected non-cases are not classified", {
-  nc <- non_cases() |>
+  nc <- non_cases()
+
+  nc <- nc |>
+    # To try to fix a CRAN check error because of a DuckDB connection
+    # issue.
+    rlang::set_names(paste0("nc_", names(nc))) |>
     purrr::map(duckplyr::as_duckdb_tibble) |>
     purrr::map(duckplyr::as_tbl)
 
   actual <- classify_diabetes(
-    kontakter = nc$kontakter,
-    diagnoser = nc$diagnoser,
-    lpr_diag = nc$lpr_diag,
-    lpr_adm = nc$lpr_adm,
-    sysi = nc$sysi,
-    sssy = nc$sssy,
-    lab_forsker = nc$lab_forsker,
-    bef = nc$bef,
-    lmdb = nc$lmdb
+    kontakter = nc$nc_kontakter,
+    diagnoser = nc$nc_diagnoser,
+    lpr_diag = nc$nc_lpr_diag,
+    lpr_adm = nc$nc_lpr_adm,
+    sysi = nc$nc_sysi,
+    sssy = nc$nc_sssy,
+    lab_forsker = nc$nc_lab_forsker,
+    bef = nc$nc_bef,
+    lmdb = nc$nc_lmdb
   ) |>
     dplyr::filter(grepl("^.._", .data$pnr)) |>
     dplyr::collect()
