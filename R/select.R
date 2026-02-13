f35401d0fb3f4fce058988fe6a4c408ca76dacad
Anders Aasted Isaksen <aasted87@gmail.com>
2026-02-13 11:53:30 +0100

    removed check of input data variable types.

--- R/select.R
+++ R/select.R
@@ -13,17 +13,14 @@
 #' @keywords internal
 select_required_variables <- function(
   data,
-  register,
-  call = rlang::caller_env()
+  register
 ) {
   checkmate::assert_choice(register, get_register_abbrev())
   expected_variables <- get_required_variables(register)
 
-  data <- data |>
+  data |>
     column_names_to_lower() |>
     dplyr::select(tidyselect::all_of(expected_variables))
-
-  check_data_types(data, register, call = call)
 }
 
 #' Convert column names to lower case
@@ -40,64 +37,4 @@ column_names_to_lower <- function(data) {
   data |>
     # Inject the lowercase names into rename.
     dplyr::rename(!!!lower_column_names)
-}
-
-#' Check data types of the register variables
-#'
-#' @inheritParams get_register_abbrev
-#'
-#' @inherit select_required_variables return
-#' @keywords internal
-check_data_types <- function(data, register, call = rlang::caller_env()) {
-  checkmate::assert_choice(register, get_register_abbrev())
-
-  # Get register variables and their expected data type(s).
-  expected <- registers()[[register]]$variables |>
-    # If data_type is a list, collapse it into a single string.
-    dplyr::mutate(
-      expected_data_type = purrr::map_chr(.data$data_type, \(x) {
-        paste(x, collapse = " or ")
-      })
-    ) |>
-    dplyr::select("name", "expected_data_type")
-
-  # Get actual variables and their data types.
-  data_for_types <- data |>
-    utils::head() |>
-    dplyr::collect()
-
-  actual <- tibble::tibble(
-    name = colnames(data_for_types),
-    actual_data_type = purrr::map_chr(data_for_types, class)
-  )
-
-  # Get mismatched data types.
-  mismatched <- actual |>
-    #  Remove columns in data we don't have expectations to.
-    dplyr::inner_join(expected, by = "name") |>
-    dplyr::filter(
-      !stringr::str_detect(.data$expected_data_type, .data$actual_data_type)
-    )
-
-  if (nrow(mismatched) > 0) {
-    expected_str <- paste0(
-      mismatched$name,
-      " = ",
-      mismatched$expected_data_type
-    )
-    actual_str <- paste0(
-      mismatched$name,
-      " = ",
-      mismatched$actual_data_type
-    )
-    cli::cli_abort(
-      c(
-        "The register {.val {register}} has unexpected data types that need to be changed.",
-        "i" = "Expected: {.val {expected_str}}.",
-        "x" = "Found: {.val {actual_str}}."
-      ),
-      call = call
-    )
-  }
-  data
 }
