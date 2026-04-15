0b052245377269378751cde0776e9e1ad0e442b7
Anders Aasted Isaksen <67263135+Aastedet@users.noreply.github.com>
2026-04-15 15:54:27 +0200

    fix: 🐛 fully use SQL in `yyww_to_yyyymmdd()` (#525)
    This is a clean PR from main identical to #498 but with meaningful
    diffs.
    
    Avoids fallback to R during keep_podiatrist_services()and potential
    DuckDB connection source errors during subsequent joins depending on the
    input data connection sources.
    
    Closes #480

--- tests/testthat/test-yyww-to-yyyymmdd.R
+++ tests/testthat/test-yyww-to-yyyymmdd.R
@@ -1,12 +1,35 @@
 test_that("date correctly converts to real dates for end and start of year", {
   # Using real dates from https://en.wikipedia.org/wiki/ISO_week_date
+  input <- data.frame(
+    date = c("0701", "0953", "0852")
+  ) |>
+    duckplyr::as_duckdb_tibble() |>
+    duckplyr::as_tbl()
+
   expected <- lubridate::ymd("2007-01-01", "2009-12-28", "2008-12-22")
-  actual <- yyww_to_yyyymmdd(c("0701", "0953", "0852"))
+
+  actual <- input |>
+    yyww_to_yyyymmdd() |>
+    dplyr::collect() |>
+    dplyr::pull("date")
+
   expect_equal(expected, actual)
 })
 
 test_that("conversion works when date is numeric", {
   # This could happen if the input was "0107" and has been converted to numeric
-  expect_equal(yyww_to_yyyymmdd(107), lubridate::ymd("2001-02-12"))
-  expect_equal(yyww_to_yyyymmdd(7), lubridate::ymd("2000-02-14"))
+  input_numeric <- data.frame(
+    date = c("701", "953", "852")
+  ) |>
+    duckplyr::as_duckdb_tibble() |>
+    duckplyr::as_tbl()
+
+  expected <- lubridate::ymd("2007-01-01", "2009-12-28", "2008-12-22")
+
+  actual_from_numeric <- input_numeric |>
+    yyww_to_yyyymmdd() |>
+    dplyr::collect() |>
+    dplyr::pull(.data$date)
+
+  expect_equal(expected, actual_from_numeric)
 })
